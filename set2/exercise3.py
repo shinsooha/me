75efe8c8386adc1c4467637a049f16a4d1024c73
Soo Ha Shin <soohashinn7@gmail.com>
2023-06-15 11:27:41 +1000

    do set 2

--- set2/exercise3.py
+++ set2/exercise3.py
@@ -261,7 +261,7 @@ def loops_7():
     This is a hard problem. Use lots of experimentation and draw
     lots of diagrams!
     """
-
+    
 
 def little_printer(some_kind_of_list, exercise_name):
     """Help to see what's going on.
