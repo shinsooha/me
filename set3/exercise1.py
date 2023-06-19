a1d3eaf82243f7553663e0496e79aa593ef099c4
Soo Ha Shin <soohashinn7@gmail.com>
2023-06-19 15:41:58 +1000

    do set 3

--- set3/exercise1.py
+++ set3/exercise1.py
@@ -17,9 +17,9 @@ def loop_ranger(start, stop=None, step=1):
     Look up for how range() works in the python docs. You could  answer this
     with just the range function, but we'd like you to do it the long way.
     """
-    return None
-
-
+    for num in range(3, 10, 2): 
+        print(num)
+    
 def two_step_ranger(start, stop):
     """Make a range that steps by 2.
 
